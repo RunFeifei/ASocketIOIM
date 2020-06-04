338924d2c1d275b82d2540851ebcfba6907c6dd2
PengFei <feifei.feixia@163.com>
2020-06-04 13:59:39 +0800

    add base

---
+++ app/src/main/java/com/run/asocketioim/base/BaseViewModel.kt
@@ -0,0 +1,39 @@
+package com.run.asocketioim.base
+
+import androidx.lifecycle.MutableLiveData
+import com.uestc.request.model.RequestViewModel
+
+
+/**
+ * Created by PengFeifei on 2018/10/27.
+ */
+
+open class BaseViewModel : RequestViewModel() {
+
+    internal val toastLiveData = MutableLiveData<String>()
+
+
+    private fun showLoading() {
+        apiLoading.postValue(true)
+    }
+
+    private fun hideLoading() {
+        apiLoading.postValue(false)
+    }
+
+    fun showToast(message: String) {
+        toastLiveData.postValue(message)
+    }
+
+    override fun onApiStart() {
+        super.onApiStart()
+    }
+
+    override fun onApiError(e: Exception?) {
+        super.onApiError(e)
+    }
+
+    override fun onApiFinally() {
+        super.onApiFinally()
+    }
+}
