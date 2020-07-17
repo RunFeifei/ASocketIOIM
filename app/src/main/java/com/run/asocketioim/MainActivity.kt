ac0a25fe64c2d9d069d9cb42b09cb4d367f90463
PengFei <feifei.feixia@163.com>
2020-07-17 18:17:33 +0800

    temp03

--- app/src/main/java/com/run/asocketioim/MainActivity.kt
+++ app/src/main/java/com/run/asocketioim/MainActivity.kt
@@ -11,14 +11,12 @@ import com.github.nkzawa.socketio.client.Socket
 import com.run.asocketioim.base.BaseActivity
 import com.run.asocketioim.base.BaseViewModel
 import kotlinx.android.synthetic.main.activity_main.*
-import java.net.URISyntaxException
+import org.json.JSONObject
 
 
 class MainActivity : BaseActivity<BaseViewModel>() {
 
     private lateinit var socket: Socket
-    private lateinit var callback: Emitter.Listener
-
 
     override fun initViewModel(): BaseViewModel {
         return BaseViewModel()
@@ -32,39 +30,119 @@ class MainActivity : BaseActivity<BaseViewModel>() {
     }
 
     override fun initPage(savedInstanceState: Bundle?) {
-        callback = Emitter.Listener {
-            it?.let {
-                if (it.isNotEmpty())
-                    Log.e("TAG-->", "onConnect--${it.size}--${it.toString()}--${it[0]})")
-            }
-        }
-        textView0.setOnClickListener {
+        connect.setOnClickListener {
             initSocketIO()
         }
-        textView.setOnClickListener {
-            socket.emit("ping")
+        disconnect.setOnClickListener {
+            socket.disconnect()
         }
-
+        my_ping.setOnClickListener {
+            socket.emit("my_ping", "my_ping")
+        }
+        my_event.setOnClickListener {
+            val json = JSONObject()
+            json.put("data", "******iuiiiuuuiuiuiuu*******")
+            json.put("count", "123")
+            socket.emit("my_event", json)
+        }
+        join_room.setOnClickListener {
+            val json = JSONObject()
+            json.put("room", "room0")
+            json.put("count", "123")
+            socket.emit("join", json)
+        }
+        leave_room.setOnClickListener {
+            val json = JSONObject()
+            json.put("room", "room0")
+            json.put("count", "123")
+            socket.emit("leave", json)
+        }
+        room_send.setOnClickListener {
+            socket.emit("testroom")
+        }
     }
 
     private fun initSocketIO() {
-        socket = IO.socket("http://10.180.5.163:${edit.text}/")
+        socket = IO.socket("http://10.180.5.163:5000/test")
         socket = socket.connect()
         Handler(Looper.getMainLooper()).postDelayed({
-            Toast.makeText(this, "${socket?.connected()}", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "${socket.connected()}", Toast.LENGTH_SHORT).show()
         }, 200)
-        try {
-            socket?.on(Socket.EVENT_CONNECT, callback)
-            socket?.on(Socket.EVENT_CONNECT_ERROR, callback)
-            socket?.on(Socket.EVENT_DISCONNECT, callback)
-            socket?.on("client", callback)
-        } catch (e: URISyntaxException) {
-            e.printStackTrace()
-        }
+        onSocket()
+    }
+
+    private fun onSocket() {
+        socket.on(Socket.EVENT_MESSAGE, Emitter.Listener {
+            if (it.isNullOrEmpty()) {
+                return@Listener
+            }
+            it[0]?.let { msg ->
+                Log.e("TAG-->", "EVENT_MESSAGE--${msg}")
+                toast("EVENT_MESSAGE--${msg}")
+            }
+        })
+        socket.on(Socket.EVENT_CONNECT, Emitter.Listener {
+            if (it.isNullOrEmpty()) {
+                return@Listener
+            }
+            if (it[0] is JSONObject) {
+                Log.e("TAG-->", "connect--${it.size}--${it.toString()}--${it[0]})")
+                toast("connect--${it.size}--${it.toString()}--${it[0]})")
+            }
+        })
+        socket.on(Socket.EVENT_CONNECT_ERROR, Emitter.Listener {
+            toast("connect_error")
+            if (it.isNullOrEmpty()) {
+                return@Listener
+            }
+            if (it[0] is JSONObject) {
+                Log.e("TAG-->", "connect_error--${it.size}--${it.toString()}--${it[0]})")
+            }
+        })
+        socket.on(Socket.EVENT_DISCONNECT, Emitter.Listener {
+            toast("disconnect")
+            if (it.isNullOrEmpty()) {
+                return@Listener
+            }
+            if (it[0] is JSONObject) {
+                Log.e("TAG-->", "disconnect--${it.size}--${it.toString()}--${it[0]})")
+            }
+        })
+        socket.on("my_response", Emitter.Listener {
+            if (it.isNullOrEmpty()) {
+                return@Listener
+            }
+            if (it[0] is JSONObject) {
+//                Log.e("TAG-->", "my_response--${it.size}--${it.toString()}--${it[0]})")
+            }
+        })
+        socket.on("my_pong", Emitter.Listener {
+            if (it.isNullOrEmpty()) {
+                return@Listener
+            }
+            if (it[0] is JSONObject) {
+                Log.e("TAG-->", "my_response--${it.size}--${it.toString()}--${it[0]})")
+                toast("my_pong--${it.size}--${it.toString()}--${it[0]})")
+            }
+        })
+        socket.on("room", Emitter.Listener {
+            if (it.isNullOrEmpty()) {
+                return@Listener
+            }
+            Log.e("TAG-->", "room get ${it[0]}")
+            toast("room get ${it[0]}")
+        })
+
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        socket?.disconnect()
+        socket.disconnect()
+    }
+
+    private fun toast(str: String) {
+        Handler(Looper.getMainLooper()).post {
+            Toast.makeText(this, str, Toast.LENGTH_SHORT).show()
+        }
     }
 }
